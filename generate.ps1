332e9b02791b60ef0c4ee42aced9bdfb6fab44ad
Daniel Eckert <dan@drakontas.org>
2020-08-02 19:20:41 -0400

    Move and optimize data/hash generate script

--- generate.ps1
+++
@@ -1,8 +0,0 @@
-$dataFileSizes = 1048576, 10485760, 104857600, 524288000, 1048576000
-
-foreach ($size in $dataFileSizes) {
-	del $size
-	(Get-FileHash .\1048576.bin -Algorithm SHA256 | Select-Object Hash) -Split "`n" -Replace "@{Hash=","" -Replace "}","" > 1048576.sha256
-	(Get-FileHash .\1048576.bin -Algorithm SHA1 | Select-Object Hash) -Split "`n" -Replace "@{Hash=","" -Replace "}","" > 1048576.sha1
-	(Get-FileHash .\1048576.bin -Algorithm MD5 | Select-Object Hash) -Split "`n" -Replace "@{Hash=","" -Replace "}","" > 1048576.md5
-}
