d7c93db6380cf6f9db9192a12db75752d9f15ee8
Daniel Eckert <dan@drakontas.org>
2020-08-05 21:34:41 -0400

    Added output to mirror-setup with instructions for end user

--- docs/mirror-setup/mirror-setup.py
+++ docs/mirror-setup/mirror-setup.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import subprocess
 import sys
@@ -35,7 +36,7 @@ packages = ["requests", "wget"]
 for package in packages:
 	install(package)
 
-# Download the latest version of mirror-sync
+# # Download the latest version of mirror-sync to the current folder
 clientFileName = "mirror-sync.py"
 try:
 	downloadFile(clientFileName)
@@ -43,10 +44,39 @@ try:
 except:
 	print("Failed to download " + clientFileName)
 
-# Download the latest version of mirror-update
+# Download the latest version of mirror-update to the current folder
 clientFileName = "mirror-update.py"
 try:
 	downloadFile(clientFileName)
 	print("Downloaded " + clientFileName)
 except:
 	print("Failed to download " + clientFileName)
+
+print(" ")
+print("binfiles.net initial mirror installation is complete.")
+print("Initial mirror file download is beinning momentarily.")
+print("This initial download will take a few seconds to a few minutes depending on uplink speed.")
+
+# Run mirror-sync for the first time to download all mirror data to the current folder
+os.system("py mirror-sync.py")
+
+print(" ")
+print("binfiles.net mirror installation and initial mirror data download are complete.")
+print(" ")
+print("In the current working direction are the scripts necessary to:")
+print(" * Download all mirror data files and hash files (mirror-sync.py)")
+print(" * Update the mirror-sync script to the latest version (mirror-update.py)")
+print(" ")
+print("We recommend you setup a cron job to run mirror-sync.py regularly.")
+print(" ")
+print("When mirror-sync.py runs, it will delete files not needed for the mirror from")
+print("the current folder. It will also check the file hashes of the data files and")
+print("re-download any data files whose hashes are inconsistent. At this same time,")
+print("any files added to the mirror-list will be downloaded along with their hashes.")
+print("Files will not be re-downloaded unless inconsistencies are detected.")
+print(" ")
+print("The mirror-update.py script will download the latest version of mirror-sync.py.")
+print("You can review the contents of mirror-sync.py script that will be downloaded:")
+print("")
+print("https://s3.binfiles.net/mirror-sync.py")
+print("")
