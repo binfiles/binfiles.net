56e6acda766fc090c407bf32a53bbb7bba2c6c9a
Daniel Eckert <dan@drakontas.org>
2020-08-05 20:41:55 -0400

    Added version tag, self-updater, and improved error handling (mirror-sync)

--- docs/mirror-setup/mirror-sync.py
+++ docs/mirror-setup/mirror-sync.py
@@ -6,6 +6,8 @@ import wget
 from os import path
 from urllib.request import urlopen, Request
 
+# mirror-sync version: 2020-08-05(a)
+
 # Describe the location of the mirror file list on the central distribution server
 serverBaseURL = "https://s3.binfiles.net/"
 serverFileList = "mirror-file-list.json"
@@ -18,11 +20,18 @@ validFileNames = []
 
 # Helper function to download large files in streams
 def downloadFile(filename):
+	
+	# Attempt the download
 	response = requests.get(serverBaseURL + filename, stream = True)
-	file = open("./" + filename,"wb")
-	for chunk in response.iter_content(chunk_size=1024):
-		file.write(chunk)
-	file.close()
+
+	# If the download succeeded, write the data to a local file of the same name
+	if response.status_code == 200:
+		file = open("./" + filename,"wb")
+		for chunk in response.iter_content(chunk_size=1024):
+			file.write(chunk)
+		file.close()
+	else:
+		raise Exception("Download failed (Error " + str(response.status_code) + "): " + serverBaseURL + filename)
 
 # Iterate over all files in the mirror list, check existence and validity, and download if needed
 for remoteDataFile in response["files"]:
@@ -123,3 +132,11 @@ for localFile in localFiles:
 
 	else:
 		print("    File has a *.py extension")
+
+# Download the latest copy of mirror-sync
+clientFileName = "mirror-sync.py"
+try:
+	downloadFile(clientFileName)
+	print("Downloaded " + clientFileName)
+except:
+	print("Failed to download " + clientFileName)
